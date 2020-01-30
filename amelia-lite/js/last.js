b80722ae278f9da754cd3792e31cc56f2f3943fb
rizky redjo <celebgramme.dev@gmail.com>
2020-01-30 15:33:22 +0700

    hapus freshchat

--- amelia-lite/js/last.js
+++
@@ -1,7 +0,0 @@
-  function initFreshChat() {
-    window.fcWidget.init({
-      token: "660fa27c-cfa6-4fa3-b6c9-cd41aad6ab87",
-      host: "https://wchat.freshchat.com"
-    });
-  }
-  function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
